153905c48bd7d7c4cbf0ec4a55f3442a194a5b61
Fickri <fickofickri@gmail.com>
2026-01-14 10:53:54 +0700

    Adding .env configuration

--- api-gateway/src/main/java/com/fickri/util/JwtUtil.java
+++ api-gateway/src/main/java/com/fickri/util/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.fickri.util;
 
 import java.security.Key;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
@@ -11,11 +13,16 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtUtil {
 
-    public static final String SECRET = "24324354654576834299855565535353223454545626262677899991123431244";
+    //public static final String SECRET = "24324354654576834299855565535353223454545626262677899991123431244";
 
+    private final String jwtSecret;
+
+    public JwtUtil(@Value("${jwt.secret}") String jwtSecret) {
+        this.jwtSecret = jwtSecret;
+    }
 
     public Key getSignKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET); 
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret); 
         return Keys.hmacShaKeyFor(keyBytes);
     } 
 
--- auth-service/src/main/java/com/fickri/service/JwtService.java
+++ auth-service/src/main/java/com/fickri/service/JwtService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
@@ -16,15 +17,24 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JwtService {
 
-    public static final String SECRET = "24324354654576834299855565535353223454545626262677899991123431244";
+    //public static final String SECRET = "24324354654576834299855565535353223454545626262677899991123431244";
+    private final String jwtSecret;
+    private final long jwtExpiration;
 
+    public JwtService(
+        @Value("${jwt.secret}") String jwtSecret,
+        @Value("${jwt.expiration}") long jwtExpiration
+    ) {
+        this.jwtSecret = jwtSecret;
+        this.jwtExpiration = jwtExpiration;
+    }
     public String generateToken(String userName){
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
     }
 
     public Key getSignKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET); 
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret); 
         return Keys.hmacShaKeyFor(keyBytes);
     } 
 
@@ -33,7 +43,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
